80fe09acb878fdce63e3d6ed5456b03c3085ba7b
Abdalrhman Emad Saad <a.emad@codeclouders.com>
2025-05-16 06:57:26 +0300

    Test: Setup initial testing enviroment with PHPUnit configuration.

---
+++ tests/TestCase.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Kettasoft\Filterable\Tests;
+
+use Orchestra\Testbench\TestCase as BaseTestCase;
+
+class TestCase extends BaseTestCase
+{
+  public function setUp(): void
+  {
+    parent::setUp();
+
+    $this->migrate();
+  }
+
+  protected function getPackageProviders($app)
+  {
+    return [\Kettasoft\Filterable\Providers\FilterableServiceProvider::class];
+  }
+
+  protected function getEnvironmentSetUp($app)
+  {
+    $app['config']->set('cache.default', 'array');
+    $app['config']->set('database.default', 'testing');
+    $app['config']->set('database.connections.testing', [
+      'driver' => 'sqlite',
+      'database' => ':memory:',
+      'prefix' => '',
+    ]);
+  }
+
+  public function migrate()
+  {
+    $migrations = [];
+
+    foreach ($migrations as $migration) {
+      (new $migration)->up();
+    }
+  }
+}
