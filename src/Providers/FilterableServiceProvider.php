c4c186a525a85a282d6640bff670d4720d355dca
Abdalrhman Emad Saad <a.emad@codeclouders.com>
2025-05-16 06:20:40 +0300

    Feat: Add FilterableServiceProvider class and publishable config file.

---
+++ src/Providers/FilterableServiceProvider.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Kettasoft\Filterable\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class FilterableServiceProvider extends ServiceProvider
+{
+  /**
+   * Bootstrap any application services.
+   *
+   * @return void
+   */
+  public function boot()
+  {
+    $this->publishes([
+      __DIR__ . '/../../config/filterable.php' => config_path('filterable.php'),
+    ], 'config');
+  }
+
+  /**
+   * Register any application services.
+   *
+   * @return void
+   */
+  public function register()
+  {
+    $this->mergeConfigFrom(
+      __DIR__ . '/../../config/filterable.php',
+      'filterable'
+    );
+  }
+
+  /**
+   * Register the generator command.
+   *
+   * @return void
+   */
+  protected function registerCommands()
+  {
+    //
+  }
+}
