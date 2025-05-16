06e6c49fc77e7e7e0f825c812232c0666fb05998
Abdalrhman Emad Saad <a.emad@codeclouders.com>
2025-05-16 08:45:02 +0300

    feat: add initial engine configurations schema

--- config/filterable.php
+++ config/filterable.php
@@ -2,15 +2,83 @@
 
 return [
     /*
-     |--------------------------------------------------------------------------
-     | Eloquent Filter Settings
-     |--------------------------------------------------------------------------
-     |
-     | This is the namespace all you Eloquent Model Filters will reside
-     |
-     */
+    |--------------------------------------------------------------------------
+    | Eloquent Filter Settings
+    |--------------------------------------------------------------------------
+    |
+    | This is the namespace all you Eloquent Model Filters will reside
+    |
+    */
 
     'namespace' => 'App\\Http\\Filters\\',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Default Filter Engine
+    |--------------------------------------------------------------------------
+    |
+    | The filter engine that will be used by default when no engine is specified
+    | explicitly. You can change it to any of the engines listed in the
+    | "engines" section below.
+    |
+    */
+    'default_engine' => 'invokeable',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Filter Engines
+    |--------------------------------------------------------------------------
+    |
+    | Define all available filter engines in your application. Each engine
+    | contains its own options that control its behavior and logic.
+    | You can create your own custom engines and register them here.
+    |
+    */
+    'engines' => [
+        /*
+        |--------------------------------------------------------------------------
+        | Invokeable Filter Engine
+        |--------------------------------------------------------------------------
+        |
+        | The Invokeable Engine provides a powerful way to dynamically map incomming reuqest parameters to corresponding methods in a filter class.
+        |
+        */
+        'invokeable' => [],
+
+        /*
+        |--------------------------------------------------------------------------
+        | Tree Filter Engine
+        |--------------------------------------------------------------------------
+        |
+        | This engine uses a tree-like structure to combine conditions using
+        | logical operators (AND/OR). It's useful for building complex queries
+        | with nested conditions.
+        |
+        */
+        'tree' => [],
+
+        /*
+        |--------------------------------------------------------------------------
+        | Ruleset Filter Engine
+        |--------------------------------------------------------------------------
+        |
+        | A simple engine that applies a simple queries independently. This
+        | is great when your filters are not deeply nested or hierarchical.
+        |
+        */
+        'ruleset' => [],
+
+        /*
+        |--------------------------------------------------------------------------
+        | SQL Expression Filter Engine
+        |--------------------------------------------------------------------------
+        |
+        | Converts filters into raw SQL expressions. Ideal when you need
+        | fine-grained control over generated SQL queries.
+        |
+        */
+        'expression' => []
+    ],
 
     /*
     |--------------------------------------------------------------------------
@@ -21,9 +89,9 @@ return [
     | you can enter the path to your own at this point
     |
     */
-       'generator' => [
-           'stub' => base_path('vendor/kettasoft/filterable/src/stubs/filter.stub'),
-       ],
+    'generator' => [
+        'stub' => base_path('vendor/kettasoft/filterable/src/stubs/filter.stub'),
+    ],
 
     /*
     |--------------------------------------------------------------------------
