58b0796dcb819e922f87d74fdd7a1a188d932eda
Abdalrhman Emad Saad <a.emad@codeclouders.com>
2025-05-20 12:54:53 +0300

    test: add test case for filter class generation command

---
+++ tests/Feature/Commands/MakeFilterCommandTest.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Kettasoft\Filterable\Tests\Feature\Commands;
+
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\File;
+use Kettasoft\Filterable\Tests\TestCase;
+
+class MakeFilterCommandTest extends TestCase
+{
+
+  /**
+   * Setup the test environment.
+   *
+   * @return void
+   */
+  public function setUp(): void
+  {
+    parent::setUp();
+
+    config()->set('filterable.generator.stubs', '/var/www/html/filterable/stubs/');
+  }
+
+  /**
+   * Clean up the testing environment before the next test.
+   *
+   * @return void
+   */
+  protected function tearDown(): void
+  {
+    File::deleteDirectory(config('filterable.save_filters_at'));
+
+    parent::tearDown();
+  }
+
+  /**
+   * It creates basic filter file.
+   * @test
+   */
+  public function it_creates_basic_filter_file()
+  {
+    $filename = 'UserFilter';
+
+    $result = Artisan::call("make:filter", [
+      "name" => $filename
+    ]);
+
+    $this->assertEquals(1, $result);
+    $this->assertTrue(File::exists(app_path('Http/Filters') . "/$filename.php"));
+  }
+
+  /**
+   * It creates filter with methods file
+   * @test
+   */
+  public function it_creates_filter_with_methods_file()
+  {
+    $filename = 'UserFilter';
+
+    $result = Artisan::call("make:filter", [
+      "name" => $filename,
+      '--filters' => 'methods'
+    ]);
+
+    $this->assertEquals(1, $result);
+    $this->assertTrue(File::exists(app_path('Http/Filters') . "/$filename.php"));
+  }
+}
