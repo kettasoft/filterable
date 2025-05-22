40473f8ced5b361338974b21363eb1c53e52f5e7
Abdalrhman Emad Saad <a.emad@codeclouders.com>
2025-05-22 20:34:22 +0300

    test: add tests for Tree engine

---
+++ tests/Unit/Engines/TreeEngineTest.php
@@ -0,0 +1,336 @@
+<?php
+
+namespace Kettasoft\Filterable\Tests\Unit\Engines;
+
+use Kettasoft\Filterable\Filterable;
+use Illuminate\Support\Facades\Config;
+use Kettasoft\Filterable\Engines\Tree;
+use Kettasoft\Filterable\Tests\TestCase;
+use Kettasoft\Filterable\Tests\Models\Tag;
+use Kettasoft\Filterable\Tests\Models\Post;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Http\Request;
+use Kettasoft\Filterable\Exceptions\InvalidDataFormatException;
+use Kettasoft\Filterable\Exceptions\InvalidOperatorException;
+use Kettasoft\Filterable\Exceptions\NotAllowedFieldException;
+use Symfony\Component\HttpFoundation\InputBag;
+
+class TreeEngineTest extends TestCase
+{
+  use RefreshDatabase;
+
+  protected $request;
+
+  public function setUp(): void
+  {
+    parent::setUp();
+
+    $total = 15;
+
+    Post::factory($total)->create([
+      'status' => 'stopped',
+    ]);
+
+    Post::factory($total)->create([
+      'status' => 'active',
+      'content' => null
+    ]);
+
+    Post::factory($total)->create([
+      'status' => 'pending',
+      'content' => null
+    ]);
+
+    Tag::factory()->create([
+      'post_id' => 1,
+      'name' => 'stopped'
+    ]);
+
+    config()->set('filterable.default_engine', 'tree');
+
+    $this->request = Request::capture()->setJson(new InputBag([
+      "filter" => [
+        "and" => [
+          [
+            "field" => "status",
+            "operator" => "eq",
+            "value" => "stopped"
+          ],
+          ['or' => []]
+        ]
+      ]
+    ]));
+  }
+
+  /**
+   * @test
+   */
+  public function it_use_tree__engine_with_simple_filtering()
+  {
+    $filter = Filterable::create($this->request)
+      ->setAllowedFields(['*'])
+      ->apply(Post::query());
+
+    $this->assertEquals(15, $filter->count());
+  }
+
+  /**
+   * @test
+   */
+  public function it_use_tree_engine_with_force_set_data()
+  {
+    $filter = Filterable::create()
+      ->setData($this->request->json()->all())
+      ->setAllowedFields(['*'])
+      ->apply(Post::query());
+
+    $this->assertEquals(15, $filter->count());
+  }
+
+  /**
+   * @test
+   */
+  public function it_use_tree_based_engin_with_field_mapping()
+  {
+    $filter = Filterable::create($this->request)
+      ->setFieldsMap(['filter_by_status' => 'status'])
+      ->setAllowedFields(['*'])
+      ->apply(Post::query());
+
+    $this->assertEquals(15, $filter->count());
+  }
+
+  /**
+   * It filter with tree based engin and enable strict mode option.
+   * @test
+   */
+  public function it_make_filter_with_tree_engine_and_enable_strict_mode_globally()
+  {
+    Config::set('filterable.engines.tree.options.strict', true);
+
+    $this->assertThrows(function () {
+      Filterable::create($this->request)
+        ->setAllowedFields([])
+        ->apply(Post::query());
+    }, NotAllowedFieldException::class);
+
+    // Try after define allowed fields.
+    $filter = Filterable::create($this->request)
+      ->setAllowedFields(['status'])
+      ->apply(Post::query());
+
+    $this->assertEquals(15, $filter->count());
+  }
+
+  /**
+   * It filter with tree based engin and enable strict mode option.
+   * @test
+   */
+  public function it_make_filter_with_tree_engine_and_enable_strict_mode_locally()
+  {
+
+    Config::set('filterable.engines.tree.strict', false);
+
+    $this->assertThrows(function () {
+      Filterable::create($this->request)
+        ->strict()
+        ->setAllowedFields([])
+        ->apply(Post::query());
+    }, NotAllowedFieldException::class);
+
+    // Try after define allowed fields.
+    $filter = Filterable::create($this->request)
+      ->strict()
+      ->setAllowedFields(['status'])
+      ->apply(Post::query());
+
+    $this->assertEquals(15, $filter->count());
+  }
+
+  /**
+   * It filter with tree based engin and enable strict mode option.
+   * @test
+   */
+  public function it_can_filter_with_allowed_operators_only()
+  {
+    $data = [
+      "filter" => [
+        "and" => [
+          ["field" => "status", "operator" => "eq", "value" => "pending"],
+          ['or' => []]
+        ]
+      ]
+    ];
+
+    // Try after define allowed fields.
+    $filter = Filterable::create()->strict()
+      ->allowdOperators(['eq'])
+      ->setAllowedFields(['status'])
+      ->setData($data)
+      ->apply(Post::query());
+
+    $this->assertEquals(15, $filter->count());
+  }
+
+  /**
+   * It filter with tree based engin and enable strict mode option.
+   * @test
+   */
+  public function it_cant_filter_with_tree_engine_and_not_allowed_operator()
+  {
+    $data = [
+      "filter" => [
+        "and" => [
+          ["field" => "status", "operator" => "like", "value" => "pending"],
+          ['or' => []]
+        ]
+      ]
+    ];
+
+    $this->assertThrows(function () use ($data) {
+      Filterable::create()
+        ->strict()
+        ->setAllowedFields(['*'])
+        ->allowdOperators(['eq'])
+        ->setData($data)
+        ->apply(Post::query());
+    }, InvalidOperatorException::class);
+
+    // Try after define allowed operator.
+    $filter = Filterable::create()
+      ->strict()
+      ->setAllowedFields(['status'])
+      ->allowdOperators(['like'])
+      ->setData($data)
+      ->apply(Post::query());
+
+    $this->assertEquals(15, $filter->count());
+  }
+
+  /**
+   * It filter with tree based engin and enable strict mode option.
+   * @test
+   */
+  public function it_can_use_default_operator_when_receved_operator_is_not_allowed_with_permissive_option()
+  {
+    $data = [
+      "filter" => [
+        "and" => [
+          ["field" => "status", "operator" => "like", "value" => "pending"],
+          ['or' => []]
+        ]
+      ]
+    ];
+
+    $filter = Filterable::create()
+      ->permissive()
+      ->setAllowedFields(['*'])
+      ->allowdOperators(['in'])
+      ->setData($data)
+      ->apply(Post::query());
+
+    $this->assertEquals(15, $filter->count());
+  }
+
+  /**
+   * It filter with tree based engin and enable strict mode option.
+   * @test
+   */
+  public function it_can_use_default_operator_when_receved_operator_is_null_with_permissive_option()
+  {
+    $data = [
+      "filter" => [
+        "and" => [
+          ["field" => "status", "operator" => null, "value" => "pending"],
+          ['or' => []]
+        ]
+      ]
+    ];
+
+    $filter = Filterable::create()
+      ->permissive()
+      ->setAllowedFields(['*'])
+      ->setData($data)
+      ->apply(Post::query());
+
+    $this->assertEquals(15, $filter->count());
+  }
+
+  /**
+   * It filter with tree based engin and enable strict mode option.
+   * @test
+   */
+  public function it_throw_error_when_data_is_incorrectly()
+  {
+    $data = [
+      "filter" => [
+        "and" => [
+          ["incorrectly" => "status", "value" => "pending"],
+          ['or' => []]
+        ]
+      ]
+    ];
+
+    $this->assertThrows(function () use ($data) {
+      Filterable::create()
+        ->permissive()
+        ->setAllowedFields(['*'])
+        ->setData($data)
+        ->apply(Post::query());
+    }, InvalidDataFormatException::class);
+  }
+
+  /**
+   * It filter with tree based engin and enable strict mode option.
+   * @test
+   */
+  public function it_filter_with_tree_based_engin_relations_and_allowed_specific_relation_path()
+  {
+    $data = [
+      "filter" => [
+        "and" => [
+          // ["field" => "status", "operator" => "eq", "value" => "stopped"],
+          ["field" => "tags.name", "operator" => "eq", "value" => "stopped"],
+          ['or' => []]
+        ]
+      ]
+    ];
+
+    // Config::set('filterable.engines.tree.strict', true);
+
+    // Try after define allowed fields.
+    $filter = Filterable::create()
+      ->setRelations(['tags' => ['name']])
+      ->setAllowedFields(['status'])
+      ->setData($data)
+      ->apply(Post::query());
+
+    $this->assertEquals(1, $filter->count());
+  }
+
+  /**
+   * It filter with tree based engin and enable strict mode option.
+   * @test
+   */
+  public function it_can_filter_with_or_and_logical_operator()
+  {
+    $data = [
+      "filter" => [
+        "and" => [
+          ["field" => "status", "operator" => "eq", "value" => "stopped"],
+          ['or' => [
+            ["field" => "status", "operator" => "eq", "value" => "active"],
+            ["field" => "status", "operator" => "eq", "value" => "pending"],
+          ]]
+        ]
+      ]
+    ];
+
+    $filter = Filterable::create()
+      ->setData($data, true)
+      ->setAllowedFields(['*'])
+      ->apply(Post::query());
+
+    $this->assertEquals(45, $filter->count());
+  }
+}
