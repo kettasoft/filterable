06644d9cbb079e5d768cd5cc72125fe9ccec2c6b
Abdalrhman Emad Saad <a.emad@codeclouders.com>
2025-05-28 12:07:23 +0300

    test: ensure model can be used to resolve query builder in filter

---
+++ tests/Unit/Filterable/ModelToBuilderResolutionTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Kettasoft\Filterable\Tests\Unit\Filterable;
+
+use Illuminate\Contracts\Database\Eloquent\Builder;
+use Kettasoft\Filterable\Filterable;
+use Kettasoft\Filterable\Tests\Models\Post;
+use Kettasoft\Filterable\Tests\TestCase;
+
+class ModelToBuilderResolutionTest extends TestCase
+{
+  /**
+   * It can inject query builder from model string.
+   * @test
+   */
+  public function it_can_inject_query_builder_from_model_string()
+  {
+    $filter = Filterable::create()->setModel(Post::class);
+
+    $this->assertInstanceOf(Builder::class, $filter->apply());
+  }
+
+  /**
+   * Summary of it_can_filtering_with_inject_model_as_instance
+   * @test
+   */
+  public function it_can_inject_query_builder_from_model_instance()
+  {
+    $filter = Filterable::create()->setModel(new Post);
+
+    $this->assertInstanceOf(Builder::class, $filter->apply());
+  }
+
+  /**
+   * Summary of it_can_filtering_with_inject_model_as_by_class
+   * @test
+   */
+  public function it_can_inject_query_builder_from_model_using_custom_class()
+  {
+    $filter = new class extends Filterable {
+      protected $model = Post::class;
+    };
+
+    $this->assertInstanceOf(Builder::class, $filter->apply());
+  }
+}
