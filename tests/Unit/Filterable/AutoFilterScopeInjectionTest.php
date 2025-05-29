0fa473b47ee5f794cbdfdcfc086a4ffd9709783f
Abdalrhman Emad Saad <a.emad@codeclouders.com>
2025-05-29 22:40:04 +0300

    test: ensure filter scope is injected automatically without requiring HasFilterable trait

---
+++ tests/Unit/Filterable/AutoFilterScopeInjectionTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Kettasoft\Filterable\Tests\Unit\Filterable;
+
+use Illuminate\Contracts\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
+use Kettasoft\Filterable\Filterable;
+use Kettasoft\Filterable\Providers\AutoRegisterFilterableServiceProvider;
+use Kettasoft\Filterable\Tests\TestCase;
+
+class AutoFilterScopeInjectionTest extends TestCase
+{
+  protected function getPackageProviders($app)
+  {
+    return [AutoRegisterFilterableServiceProvider::class, ...parent::getPackageProviders($app)];
+  }
+
+  /**
+   * It test filter scope is available without trait.
+   * @test
+   */
+  public function ittest_filter_scope_is_available_without_trait()
+  {
+    $this->assertInstanceOf(Builder::class, $this->model()->filter(Filterable::create()));
+  }
+
+  protected function model()
+  {
+    $model = new class extends Model {};
+
+    return $model;
+  }
+}
