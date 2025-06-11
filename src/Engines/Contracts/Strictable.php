8f76999bde6fcd34fb32a5bb1d07b82421ff4c3f
Abdalrhman Emad Saad <a.emad@codeclouders.com>
2025-06-11 10:20:01 +0300

    refactor: rename isStrict method description

--- src/Engines/Contracts/Strictable.php
+++ src/Engines/Contracts/Strictable.php
@@ -5,7 +5,7 @@ namespace Kettasoft\Filterable\Engines\Contracts;
 interface Strictable
 {
   /**
-   * Check if engine enable strict mode.
+   * Check if the strict mode is enable.
    * @return bool
    */
   public function isStrict(): bool;
