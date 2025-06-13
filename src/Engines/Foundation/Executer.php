6c7a1056874a2bc1ec3c8ec9fc0ba9407a38a55a
Abdalrhman Emad Saad <a.emad@codeclouders.com>
2025-06-13 09:15:59 +0300

    refactor: replace EngineRunner with new Executer class

---
+++ src/Engines/Foundation/Executer.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Kettasoft\Filterable\Engines\Foundation;
+
+use Illuminate\Database\Eloquent\Builder;
+use Kettasoft\Filterable\Engines\Contracts\Executable;
+
+trait Executer
+{
+  /**
+   * Execute the given Executable instance with the provided query builder instance.
+   * @param \Kettasoft\Filterable\Engines\Contracts\Executable $executable
+   * @param \Illuminate\Database\Eloquent\Builder $builder
+   * @return \Illuminate\Database\Eloquent\Builder
+   */
+  public static function execute(Executable $executable, Builder $builder)
+  {
+    return $executable->execute($builder);
+  }
+}
