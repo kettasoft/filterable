ddc9b811cb0d9f206362b979250df692d8303310
Abdalrhman Emad Saad <a.emad@codeclouders.com>
2025-09-03 18:10:53 +0300

    feat: add AttributeRegistry for managing attribute handlers

---
+++ src/Engines/Foundation/Attributes/AttributeRegistry.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Kettasoft\Filterable\Engines\Foundation\Attributes;
+
+use ReflectionMethod;
+use Kettasoft\Filterable\Filterable;
+use Kettasoft\Filterable\Engines\Foundation\Attributes\Handlers\Contracts\AttributeHandlerInterface;
+
+class AttributeRegistry
+{
+  /**
+   * @var array<string, AttributeHandlerInterface>
+   */
+  protected array $handlers = [];
+
+  /**
+   * Register an attribute handler.
+   * @param string $attributeClass
+   * @param AttributeHandlerInterface $handler
+   * @return void
+   */
+  public function register(string $attributeClass, AttributeHandlerInterface $handler): void
+  {
+    $this->handlers[$attributeClass] = $handler;
+  }
+
+  /**
+   * Get handlers for the given method of a filterable class.
+   *
+   * @param Filterable $filterable
+   * @param string $method
+   * @return array<int, array{0: string, 1: object}>
+   */
+  public function getHandlersForMethod(Filterable $filterable, string $method): array
+  {
+    $reflection = new ReflectionMethod($filterable, $method);
+    $attributes = $reflection->getAttributes();
+    $matchedHandlers = [];
+
+    foreach ($attributes as $attribute) {
+      $attrName = $attribute->getName();
+      if (isset($this->handlers[$attrName])) {
+        $handler = $this->handlers[$attrName];
+        $matchedHandlers[] = [$handler, $attribute->newInstance()];
+      }
+    }
+
+    return $matchedHandlers;
+  }
+}
