35ef183cf0dcb9b8f1d8853af83c66bed14a536d
kettasoft <kettasoft@gmail.com>
2025-10-12 22:49:44 +0300

    feat(EngineManager): add extend method to register custom engines

--- tests/Unit/Engines/EngineManagerTest.php
+++ tests/Unit/Engines/EngineManagerTest.php
@@ -2,13 +2,15 @@
 
 namespace Kettasoft\Filterable\Tests\Unit\Engines;
 
-use Kettasoft\Filterable\Engines\Expression;
 use Kettasoft\Filterable\Filterable;
-use Kettasoft\Filterable\Tests\TestCase;
-use Kettasoft\Filterable\Engines\Ruleset;
-use Kettasoft\Filterable\Engines\Invokeable;
-use Kettasoft\Filterable\Engines\Factory\EngineManager;
 use Kettasoft\Filterable\Engines\Tree;
+use Kettasoft\Filterable\Tests\TestCase;
+use Illuminate\Database\Eloquent\Builder;
+use Kettasoft\Filterable\Engines\Ruleset;
+use Kettasoft\Filterable\Engines\Expression;
+use Kettasoft\Filterable\Engines\Invokeable;
+use Kettasoft\Filterable\Engines\Foundation\Engine;
+use Kettasoft\Filterable\Engines\Factory\EngineManager;
 
 class EngineManagerTest extends TestCase
 {
@@ -51,5 +53,52 @@ class EngineManagerTest extends TestCase
     $engine = EngineManager::generate('invokable', new Filterable());
 
     $this->assertInstanceOf(Invokeable::class, $engine);
+  }
+
+  public function test_it_can_create_custom_engine_from_engine_manager()
+  {
+    $engine = new class (new Filterable()) extends Engine {
+
+      public function execute(Builder $builder): Builder
+      {
+        return $builder;
+      }
+
+      protected function isStrictFromConfig(): bool
+      {
+        return false;
+      }
+
+      protected function getAllowedFieldsFromConfig(): array
+      {
+        return [];
+      }
+
+      protected function isIgnoredEmptyValuesFromConfig(): bool
+      {
+        return false;
+      }
+
+      public function getEngineName(): string
+      {
+        return false;
+      }
+
+      public function defaultOperator()
+      {
+        return '=';
+      }
+
+      public function getOperatorsFromConfig(): array
+      {
+        return ['='];
+      }
+    };
+
+    EngineManager::extend('custom', get_class($engine));
+
+    $engine = EngineManager::generate('custom', new Filterable());
+
+    $this->assertInstanceOf(Engine::class, $engine);
   }
 }
