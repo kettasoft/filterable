553a12d87c076dae764b9ab0026f1d959dd97312
kettasoft <kettasoft@gmail.com>
2025-10-20 18:32:04 +0300

    test: add additional tests for accessing provided data in Filterable class

--- tests/Unit/Filterable/FilterableProvidedDataTest.php
+++ tests/Unit/Filterable/FilterableProvidedDataTest.php
@@ -30,4 +30,21 @@ class FilterableProvidedDataTest extends TestCase
         $this->assertEquals(1, $filterable->filter()->count());
         $this->assertEquals(1, $filterable->provided("post_id"));
     }
+
+    public function test_it_can_get_all_provided_data(): void
+    {
+        $filterable = Filterable::create();
+
+        $this->assertEquals(['post_id' => 1], $filterable->provided());
+    }
+
+    public function test_it_can_access_specific_provided_key(): void
+    {
+        $filterable = Filterable::create();
+
+        $this->assertTrue($filterable->hasProvided("post_id"));
+        $this->assertFalse($filterable->hasProvided("non_existing_key"));
+        $this->assertEquals(1, $filterable->provided("post_id"));
+        $this->assertEquals(null, $filterable->provided("non_existing_key"));
+    }
 }
