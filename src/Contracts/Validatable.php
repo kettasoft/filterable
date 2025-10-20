160cc9d904e82e408f49af15d5fe352572ebabbc
kettasoft <kettasoft@gmail.com>
2025-10-20 16:49:55 +0300

    refactor: add docblocks for validate and apply methods in Validatable and Appliable interfaces

--- src/Contracts/Validatable.php
+++ src/Contracts/Validatable.php
@@ -4,6 +4,17 @@ namespace Kettasoft\Filterable\Contracts;
 
 interface Validatable
 {
+  /**
+   * Validate the current request.
+   *
+   * @return void
+   */
   public function validate();
+
+  /**
+   * Get the validation rules.
+   *
+   * @return array
+   */
   public function rules(): array;
 }
