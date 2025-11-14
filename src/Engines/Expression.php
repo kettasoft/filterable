4aa3b453d6a4d1845e742af0d1a040b1d71f2cd2
kettasoft <kettasoft@gmail.com>
2025-11-15 01:06:37 +0200

    refactor: remove unused methods and imports from Expression engine

--- src/Engines/Expression.php
+++ src/Engines/Expression.php
@@ -3,18 +3,13 @@
 namespace Kettasoft\Filterable\Engines;
 
 use Kettasoft\Filterable\Support\Payload;
-use Kettasoft\Filterable\Traits\FieldNormalizer;
 use Illuminate\Contracts\Database\Eloquent\Builder;
-use Kettasoft\Filterable\Engines\Foundation\Clause;
 use Kettasoft\Filterable\Engines\Foundation\Engine;
 use Kettasoft\Filterable\Support\ConditionNormalizer;
 use Kettasoft\Filterable\Support\ValidateTableColumns;
 use Kettasoft\Filterable\Engines\Foundation\ClauseApplier;
 use Kettasoft\Filterable\Engines\Foundation\ClauseFactory;
-use Kettasoft\Filterable\Engines\Foundation\Enums\Operators;
 use Kettasoft\Filterable\Engines\Foundation\Appliers\Applier;
-use Kettasoft\Filterable\Exceptions\InvalidOperatorException;
-use Kettasoft\Filterable\Exceptions\NotAllowedFieldException;
 use Kettasoft\Filterable\Engines\Foundation\Parsers\Dissector;
 
 class Expression extends Engine
@@ -57,11 +52,6 @@ class Expression extends Engine
     return $builder;
   }
 
-  public function getAllowedFieldsFromConfig(): array
-  {
-    return config('filterable.engines.expression.allowed_fields', []);
-  }
-
   /**
    * Check if the given column is registered in schema. 
    * @param mixed $column
@@ -76,16 +66,6 @@ class Expression extends Engine
     return true;
   }
 
-  public function getOperatorsFromConfig(): array
-  {
-    return config('filterable.engines.expression.allowed_operators', []);
-  }
-
-  public function isStrictFromConfig(): bool
-  {
-    return config('filterable.engines.expression.strict', true);
-  }
-
   /**
    * Get engine default operator.
    * @return string
@@ -93,11 +73,6 @@ class Expression extends Engine
   public function defaultOperator(): string
   {
     return config('filterable.engines.expression.default_operator', 'eq');
-  }
-
-  public function isIgnoredEmptyValuesFromConfig(): bool
-  {
-    return config('filterable.engines.expression.ignore_empty_values', false);
   }
 
   /**
