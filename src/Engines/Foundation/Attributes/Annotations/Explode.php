2687ccc908aab597c0680d3c0287e3eef46ae01d
kettasoft <kettasoft@gmail.com>
2026-03-01 03:17:52 +0200

    feat(Explode): add Explode attribute for string splitting functionality

---
+++ src/Engines/Foundation/Attributes/Annotations/Explode.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Kettasoft\Filterable\Engines\Foundation\Attributes\Annotations;
+
+use Attribute;
+
+#[Attribute(Attribute::TARGET_METHOD)]
+class Explode implements \Kettasoft\Filterable\Engines\Foundation\Attributes\Contracts\MethodAttribute
+{
+  /**
+   * Constructor for Explode attribute.
+   * @param string $delimiter The delimiter to use for exploding the parameter value.
+   */
+  public function __construct(public string $delimiter = ',') {}
+
+  /**
+   * Get the stage at which this attribute should be applied.
+   *
+   * @return int
+   */
+  public static function stage(): int
+  {
+    return \Kettasoft\Filterable\Engines\Foundation\Attributes\Enums\Stage::VALIDATE->value;
+  }
+
+  /**
+   * Handle the attribute logic.
+   *
+   * @param \Kettasoft\Filterable\Engines\Foundation\Attributes\AttributeContext $context
+   * @return void
+   */
+  public function handle(\Kettasoft\Filterable\Engines\Foundation\Attributes\AttributeContext $context): void
+  {
+    /** @var \Kettasoft\Filterable\Support\Payload $payload */
+    $payload = $context->payload;
+
+    $payload->explode($this->delimiter, true);
+  }
+}
