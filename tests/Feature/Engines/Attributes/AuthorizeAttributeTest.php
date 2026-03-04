d08128153a482aa444f3ab09661bcbac9f0a2c88
kettasoft <kettasoft@gmail.com>
2026-03-04 15:12:58 +0200

    feat(Authorize): implement Authorize attribute for method-level authorization and add corresponding tests

---
+++ tests/Feature/Engines/Attributes/AuthorizeAttributeTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Kettasoft\Filterable\Tests\Feature\Engines\Attributes;
+
+use Kettasoft\Filterable\Engines\Foundation\Attributes\Annotations\Authorize;
+use Kettasoft\Filterable\Filterable;
+use Kettasoft\Filterable\Support\Payload;
+use Kettasoft\Filterable\Tests\Feature\Engines\Attributes\Authorizations\CanMakeFilter;
+use Kettasoft\Filterable\Tests\Models\Post;
+use Kettasoft\Filterable\Tests\TestCase;
+
+class AuthorizeAttributeTest extends TestCase
+{
+  public function test_authorize_attribute_can_make_filter()
+  {
+    request()->merge([
+      'tags' => 'testing',
+    ]);
+
+    $class = new class extends Filterable {
+      protected $filters = ['tags'];
+
+      #[Authorize(CanMakeFilter::class)]
+      public function tags(Payload $payload)
+      {
+        $this->builder->where('tags', $payload->value);
+      }
+    };
+
+    $sql = Post::filter($class)->toRawSql();
+
+    $this->assertStringContainsString('where "tags"', $sql);
+  }
+}
